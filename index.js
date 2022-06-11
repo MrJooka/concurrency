629e75e76654724efa4d4aff256d6e7f8f3224f8
Jooka <mr.jooka@gmail.com>
2022-06-12 04:27:18 +0900

    ㅇ

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const removeClass = (name, el) => (el.classList.remove(name), el);
 // 하나의 콜스택 아래에 있을 땐 merge(병합되어) 실행되기 때문에 그냥 hide의 상태를 브라우저가 페인팅하지 않는다
 // 디버그로 step하나씩 실행햇을 때는 css 애니메이션이 실행 잘된다. 그러나 실제 브라우저는 painting 변경에 영향을 주는 코드들은
 // 비동기적으로 여러개의 실행후 최종결과 하나로 한번만 그리기  때문이다. (이것이 효율적이다)
-// 그래서 settimeout으로 비동기로 실행해야한다
+// 그래서 settimeout으로 비동기로  callstack을 비워주고 실행하면 animation이 의도한대로 동작한다
 
 // const show = (el) => removeClass('hide', el);
 const show = (el) => setTimeout(() => removeClass('hide', el), 1);
