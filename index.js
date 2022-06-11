2ef9a41d43d9c0f366e67d71635017b3d954620d
Jooka <mr.jooka@gmail.com>
2022-06-12 04:31:26 +0900

    통신으로 데이터 받음

--- index.js
+++ index.js
@@ -35,8 +35,14 @@ const products = [
   { name: '선풍기', price: 60000, quantity: 1, selected: true },
 ];
 
+const delay = (time, a) =>
+  new Promise((resolve) => setTimeout(() => resolve(a), time));
+
 const Product = {};
 Product.list = () => products;
+
+// 통신으로 데이터를 받는다는 가정(0.7초후 데이터 받음)
+Product.list700 = () => delay(700, products);
 
 Product.list.tmpl = (products) => `
   <table>
@@ -73,13 +79,6 @@ const append = (parent, child) => parent.appendChild(child);
 
 const removeClass = (name, el) => (el.classList.remove(name), el);
 
-// 아래 show코드  실행부분은 dataFn 실행 -> templFn 실행 -> append실행 되고 나서 removeclass를 실행하는데
-// 하나의 콜스택 아래에 있을 땐 merge(병합되어) 실행되기 때문에 그냥 hide의 상태를 브라우저가 페인팅하지 않는다
-// 디버그로 step하나씩 실행햇을 때는 css 애니메이션이 실행 잘된다. 그러나 실제 브라우저는 painting 변경에 영향을 주는 코드들은
-// 비동기적으로 여러개의 실행후 최종결과 하나로 한번만 그리기  때문이다. (이것이 효율적이다)
-// 그래서 settimeout으로 비동기로  callstack을 비워주고 실행하면 animation이 의도한대로 동작한다
-
-// const show = (el) => removeClass('hide', el);
 const show = (el) => setTimeout(() => removeClass('hide', el), 1);
 const openPage = (title, dataFn, tmplFn) =>
   show(
@@ -95,5 +94,5 @@ const openPage = (title, dataFn, tmplFn) =>
   );
 
 document.addEventListener('click', () => {
-  openPage('상품 목록', Product.list, Product.list.tmpl);
+  openPage('상품 목록', Product.list700, Product.list.tmpl);
 });
